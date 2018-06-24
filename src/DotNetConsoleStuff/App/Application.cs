93f973791fa71703ddb218163b5686c2f6c99c55
DeveTho <DeveTho@users.noreply.github.com>
2018-06-24 16:27:50 +0200

    Injecting AppSettings in Application

--- src/DotNetConsoleStuff/App/Application.cs
+++ src/DotNetConsoleStuff/App/Application.cs
@@ -2,6 +2,13 @@ namespace DotNetConsoleStuff.App
 {
     public class Application : IApplication
     {
+        public Application(IAppSettings appSettings)
+        {
+            AppSettings = appSettings;
+        }
+
+        private IAppSettings AppSettings { get; }
+
         public void Run()
         {
             throw new System.NotImplementedException();
